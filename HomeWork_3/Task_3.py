602bcd697d147ec2ad2085fe3032921ba69afd26
Anton <a.s.obrevko@yandex.tu>
2022-05-29 01:09:24 +0300

    Task 3 - Completed

--- HomeWork_3/Task_3.py
+++ HomeWork_3/Task_3.py
@@ -5,3 +5,9 @@
 
 - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 """
+
+test = [1.1, 1.2, 3.1, 5, 10.01]
+fracts = [round(test[i]%1,2) for i in range(len(test))]
+print(fracts)
+result = max(fracts) - min(fracts)
+print(f"{result:.2f}")
