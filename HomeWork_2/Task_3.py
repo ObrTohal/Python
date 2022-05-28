18f1215a9a88942b15968df3886426a0ab964529
Anton <a.s.obrevko@yandex.tu>
2022-05-28 21:52:15 +0300

    Task 3 - Completed

--- HomeWork_2/Task_3.py
+++ HomeWork_2/Task_3.py
@@ -1,1 +1,13 @@
 # Реализуйте алгоритм перемешивания списка.
+
+import random
+
+def Alg_shuffle(list_of:list):
+    for num in range(0,len(list_of)):
+        list_of.insert(int(random.random()*10)%len(list_of),list_of.pop())
+    return None
+
+lis=[1,2,3,4,5,6]
+print(lis)
+Alg_shuffle(lis)
+print(lis)
