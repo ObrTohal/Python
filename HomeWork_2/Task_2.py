ce7aae3500b8e38c5c9d93788371268420fefc72
Anton <a.s.obrevko@yandex.tu>
2022-05-28 20:37:25 +0300

    Task 2 - Completed

--- HomeWork_2/Task_2.py
+++ HomeWork_2/Task_2.py
@@ -3,3 +3,18 @@
 # Пример:
 
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+N = int(input("Введите N: "))
+
+def f(N):
+    sum = 0
+    result ={}
+    for num in range(1,N+1):
+        sum+=(1+1/num)**num
+        result[num] = round(sum)
+    return result
+
+print(f(N))
+
+# result = list(map(f,range(1,N+1)))
+# print(result)
