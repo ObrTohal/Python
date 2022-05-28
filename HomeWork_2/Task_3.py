392beb4b48429a5cd8867c73589095eaf5a53b86
ObrTohal <100442663+ObrTohal@users.noreply.github.com>
2022-05-28 22:33:46 +0300

    Task 3 - Completed

--- HomeWork_2/Task_3.py
+++ HomeWork_2/Task_3.py
@@ -4,7 +4,7 @@ import random
 
 def Alg_shuffle(list_of:list):
     for num in range(0,len(list_of)):
-        list_of.insert(int(random.random()*10)%len(list_of),list_of.pop())
+        list_of.insert(int(random.random()*len(list_of))%len(list_of),list_of.pop())
     return None
 
 lis=[1,2,3,4,5,6]
